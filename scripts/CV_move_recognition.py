484761fa0dbc1b2fb7841ab29c001b056552564c
vaishakh1996 <vaishakh229210@gmail.com>
2023-03-14 16:10:41 +0100

    to display move no at end

--- scripts/CV_move_recognition.py
+++ scripts/CV_move_recognition.py
@@ -26,7 +26,8 @@ import config as cfg
 from useful_functions.move_detection_ssim_and_pcl import OccupancyChecker
 ready = False
 
-PLAYCHESS_PKG_DIR = os.path.normpath(os.path.join(os.path.dirname(__file__),'..'))
+PLAYCHESS_PKG_DIR = os.path.normpath(
+    os.path.join(os.path.dirname(__file__), '..'))
 MOVES_DATA_DIR = PLAYCHESS_PKG_DIR + '/data/moves'
 
 # Publishers initialization
@@ -108,7 +109,7 @@ class DepthProcessing:
             self.second_row = 'row7'
             self.columns_indexes = cfg.columns_black
 
-    def move_identification(self,move):
+    def move_identification(self, move):
         # Function to look for the presence of pieces over the chessboard squares.
         # chessboard: PointCloud of the isolated chessboard.
         # squares_boundboxes: a list containing the 64 bounding boxes to isolate each square and the things inside them.
@@ -127,8 +128,9 @@ class DepthProcessing:
 
         opponent_move = OccupancyChecker()
         self.possible_capture, self.possible_start_square, self.possible_end_square, self.possible_castle, self.possible_enpasent, self.possible_promotion = opponent_move.opponent_move(
-            self.rgb_img_before, self.rgb_img_after, self.depth_arr_after,move, False)
-        print('self.possible_capture, self.possible_start_square, self.possible_end_square, self.possible_castle, self.possible_enpasent, self.possible_promotion:\n',self.possible_capture, self.possible_start_square, self.possible_end_square, self.possible_castle, self.possible_enpasent, self.possible_promotion)
+            self.rgb_img_before, self.rgb_img_after, self.depth_arr_after, move, False)
+        print('self.possible_capture, self.possible_start_square, self.possible_end_square, self.possible_castle, self.possible_enpasent, self.possible_promotion:\n',
+              self.possible_capture, self.possible_start_square, self.possible_end_square, self.possible_castle, self.possible_enpasent, self.possible_promotion)
         if len(self.possible_castle) != 0:
             castle = True
             if 'h8' in self.possible_castle:
@@ -149,7 +151,7 @@ class DepthProcessing:
             move_square = 'castle'
             moved_piece = 'castle'
             ending_square = 'castle'
-        
+
         elif len(self.possible_enpasent) != 0:
             en_passant = True
             for i in self.possible_enpasent:
@@ -159,7 +161,7 @@ class DepthProcessing:
                         moved_piece = self.live_chessboard_situation[i][0]
                     elif i[1] == '6':
                         ending_square = i
-                    elif i[1] =='5' and self.live_chessboard_situation[i][1] == self.color:
+                    elif i[1] == '5' and self.live_chessboard_situation[i][1] == self.color:
                         en_passant_square = i
                 elif self.opposite_color == 'black':
                     if i[1] == '4' and self.live_chessboard_situation[i][1] == self.opposite_color:
@@ -167,7 +169,7 @@ class DepthProcessing:
                         moved_piece = self.live_chessboard_situation[i][0]
                     elif i[1] == '3':
                         ending_square = i
-                    elif i[1] =='4' and self.live_chessboard_situation[i][1] == self.color:
+                    elif i[1] == '4' and self.live_chessboard_situation[i][1] == self.color:
                         en_passant_square = i
 
         elif len(self.possible_promotion) != 0:
@@ -205,28 +207,28 @@ class DepthProcessing:
         #             print('start squares option :',self.possible_start_square)
         #             move_square = input('I am confused!\nPls enter start square :\n')
         #             moved_piece = self.live_chessboard_situation[move_square][0]
-            
+
         #     else:
         #         move_square = input('I am confused!\nPls enter start square :\n')
         #         moved_piece = self.live_chessboard_situation[move_square][0]
-            
+
         #     if len(self.possible_end_square)!=0:
         #         if len(self.possible_end_square)==1:
         #             ending_square = self.possible_end_square[0]
         #         else:
         #             print('end square option:',self.possible_end_square)
         #             ending_square = input('I am confused!\nPls enter end square :\n')
-            
+
         #     elif len(self.possible_end_square)==0 and len(self.possible_capture)==0:
         #         ending_square = input('I am confused!\nPls enter end square :\n')
-            
+
         #     elif len(self.possible_end_square)!=0 and len(self.possible_capture)!=0:
         #         ending_square = input('I am confused!\nPls enter end square :\n')
 
         #     if len(self.possible_capture)!=0:
         #         if len(self.possible_capture)==1:
         #             ending_square = self.possible_capture[0]
-                
+
         #         else:
         #             print('end square option:',self.possible_capture)
         #             ending_square = input('I am confused!\nPls enter end square :\n')
@@ -240,25 +242,24 @@ class DepthProcessing:
         #         ending_square = self.possible_capture[0]
         #     elif len(self.possible_end_square) != 0:
         #         ending_square = self.possible_end_square[0]
-        
+
         if en_passant:
             self.live_chessboard_situation[ending_square][0] = self.live_chessboard_situation[move_square][0]
             self.live_chessboard_situation[ending_square][1] = self.live_chessboard_situation[move_square][1]
             self.live_chessboard_situation[move_square][0] = 'none'
             self.live_chessboard_situation[move_square][1] = 'none'
-            #change enpasent square to none
-            print('self.live_chessboard_situation[en_pasent_square]',self.possible_enpasent,)
+            # change enpasent square to none
+            print(
+                'self.live_chessboard_situation[en_pasent_square]', self.possible_enpasent,)
             for i in self.possible_enpasent:
-                if i != ending_square and i != move_square and i[1]=='4' and self.live_chessboard_situation[i][1] == self.color:
-                
+                if i != ending_square and i != move_square and i[1] == '4' and self.live_chessboard_situation[i][1] == self.color:
+
                     en_pasent_square = i
 
                     # self.live_chessboard_situation[en_pasent_square][0] = 'none'
                     # self.live_chessboard_situation[en_pasent_square][1] = 'none'
 
             self.found = True
-
-
 
         else:
             # updating the live_chessboard_situation
@@ -267,7 +268,6 @@ class DepthProcessing:
             # self.live_chessboard_situation[move_square][0] = 'none'
             # self.live_chessboard_situation[move_square][1] = 'none'
             self.found = True
-
 
         if castle:
             # If castle happened, send a message to correctly update the GUI.
@@ -291,29 +291,29 @@ class DepthProcessing:
 
         return moved_piece, move_square, ending_square
 
-    def identify_moved_piece(self,move):
+    def identify_moved_piece(self, move):
         global analysis_not_done_yet
 
         # Understand which piece has been moved
         start = time()
         moved_piece, start_square, end_square = self.move_identification(move
-        )
+                                                                         )
         end = time()
-        elapsed_time = end- start
+        elapsed_time = end - start
 
         print('MOVED PIECE: ' + str(moved_piece))
         print('STARTING SQUARE: ' + str(start_square))
         print('ENDING SQUARE: ' + str(end_square))
 
-
         mv_folder = MOVES_DATA_DIR + '/move_{}'.format(self.move)
         # saving the recognized move
-        with open(os.path.join(mv_folder,'recognized_move.txt'),"w") as file:
+        with open(os.path.join(mv_folder, 'recognized_move.txt'), "w") as file:
             file.write("moved piece:{}".format(str(moved_piece)))
             file.write("sratring square:{}".format(str(start_square)))
             file.write("moved piece: {}".format(str(end_square)))
-            file.write("time_taken_for_move_recognition:{}".format(str(elapsed_time)))
-            
+            file.write("time_taken_for_move_recognition:{}".format(
+                str(elapsed_time)))
+
         # Send messages regarding the move executed by the opponent to change the GUI.
         rospy.loginfo('Publishing the opponent move...')
         # print('Time for move recognition: ' +
@@ -338,7 +338,7 @@ class DepthProcessing:
             self.rgb_img_before, depth_img_before = self._get_rgb_and_depth()
             rospy.loginfo('image after tiago move acquired')
             self.move += 1
-            print('move:',self.move)
+            print('move:', self.move)
             if save:
                 mv_folder = MOVES_DATA_DIR + '/move_{}'.format(self.move)
                 try:
@@ -351,8 +351,7 @@ class DepthProcessing:
                 cv2.imwrite(mv_folder + '/depth_before.png', depth_img_before)
                 np.savetxt(mv_folder + '/depth_before.csv',
                            depth_img_before, delimiter=",")
-            
-            
+
                 # NOTE. Retrieve the depth data with depth_image = np.genfromtxt(mv_folder + '/depth_before.csv', delimiter=',')
 
         if data.data == 15:
@@ -361,13 +360,14 @@ class DepthProcessing:
             self.rgb_img_after, self.depth_arr_after = self._get_rgb_and_depth()
             if save:
                 mv_folder = MOVES_DATA_DIR + '/move_{}'.format(self.move)
-                
-                #read yaml file to dump it later
-                with open(os.path.join(PLAYCHESS_PKG_DIR,'scripts/live_chessboard_situation.yaml'), 'rb') as live_file:
-                    save_for_debug = yaml.load(live_file.read(), Loader=yaml.Loader)
+
+                # read yaml file to dump it later
+                with open(os.path.join(PLAYCHESS_PKG_DIR, 'scripts/live_chessboard_situation.yaml'), 'rb') as live_file:
+                    save_for_debug = yaml.load(
+                        live_file.read(), Loader=yaml.Loader)
                 # dump yaml fro debugin
-                with open(os.path.join(mv_folder,'live_chessboard_situation.yaml'), "w") as t_p:
-                            yaml.dump(save_for_debug, t_p)
+                with open(os.path.join(mv_folder, 'live_chessboard_situation.yaml'), "w") as t_p:
+                    yaml.dump(save_for_debug, t_p)
                 # Save the RGB and depth data AFTER the execution of the opponent's move
                 cv2.imwrite(mv_folder + '/rgb_after.png', self.rgb_img_after)
                 cv2.imwrite(mv_folder + '/depth_after.png',
@@ -377,7 +377,7 @@ class DepthProcessing:
                 # Retrieve the depth data with depth_image = np.genfromtxt(mv_folder + '/depth_after.csv', delimiter=',')
             # Identify the move and broadcast messages accordingly
             self.identify_moved_piece(self.move)
-
+            print("move:", self.move)
 
         elif data.data == 50:
             # TODO. Verify if needed, then remove.
